5a9a5d96664d5d47449a761d164deec6bc93e8ae
srivers <stephen.rivers@sjrb.ca>
2016-12-22 15:05:44 -0700

    further refactoring, make the envelope class more complete working toward remove the dimension struct

--- Enveloper/Envelope.cs
+++ Enveloper/Envelope.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Enveloper
             CutOffRadius = 0.05f;
         }
 
-        public Envelope(float length, float height, float scaling)
+        public Envelope(float length, float height, float scaling = 1.0f)
         {
             Length = length;
             Height = height;
@@ -27,20 +28,10 @@ namespace Enveloper
             CutOffRadius = 0.05f;
         }
 
-        //public Dimensions getDim()
-        //{
-        //    var dim = new Dimensions();
-        //    dim.b = roundToNearestFraction(Scale * (Length / 2) * (float)Math.Sqrt(2));
-        //    dim.c = roundToNearestFraction(Scale * Offset * 2);
-        //    dim.d = roundToNearestFraction(Scale * (Height / 2) * (float)Math.Sqrt(2));
-        //    dim.length = dim.b + dim.c + dim.d;
-        //    dim.r = CutOffRadius * Scale;
-        //    return dim;
-        //}
-
+        Dimensions dim;
         public Dimensions getDim(bool roundDimensions = false)
         {
-            var dim = new Dimensions();
+            dim = new Dimensions();
             dim.b = roundDimensions ? roundToNearestFraction(Scale * (Length / 2) * (float)Math.Sqrt(2)) : Scale * (Length / 2) * (float)Math.Sqrt(2);
             dim.c = roundDimensions ? roundToNearestFraction(Scale * Offset * 2) : Scale * Offset * 2;
             dim.d = roundDimensions ? roundToNearestFraction(Scale * (Height / 2) * (float)Math.Sqrt(2)) : Scale * (Height / 2) * (float)Math.Sqrt(2);
@@ -48,6 +39,36 @@ namespace Enveloper
             dim.r = CutOffRadius * Scale;
             return dim;
         }
+
+        List<SortedList<int, PointF>> segments;
+
+        List<SortedList<int, PointF>> calcSegments()
+        {
+            var result = new List<SortedList<int, PointF>>();
+            var segment = new SortedList<int, PointF>();
+            //segments
+            // topLeft, topRight, bottomRight and bottomLeft
+            // Each segment has 5 points
+
+            // topLeft
+            segment.Add(0,new PointF(0.0f + dim.c / 2 - 0.3f * dim.r, dim.length - dim.d - dim.c / 2 - 0.7f * dim.r));
+            segment.Add(1,new PointF(0.0f,dim.b));
+            segment.Add(2, new PointF(0.0f, 0.0f));
+            segment.Add(3, new PointF(dim.b - 0.7f * dim.r,0.0f));
+            segment.Add(4, new PointF(dim.b + dim.c / 2 - 0.7f * dim.r,dim.c / 2 - 0.3f * dim.r));
+            result.Add(segment);
+
+            //topRight
+            segment = new SortedList<int, PointF>();
+            segment.Add(0, new PointF(dim.length - dim.d - dim.c / 2 + 0.7f * dim.r, dim.c / 2 - 0.3f * dim.r));
+            segment.Add(1, new PointF(dim.length - dim.d + 0.7f * dim.r, 0.0f));
+            segment.Add(2, new PointF(dim.length, 0.0f));
+            segment.Add(3, new PointF(dim.length, dim.d - 0.7f * dim.r));
+            segment.Add(4, new PointF(dim.length - dim.c / 2 + 0.3f * dim.r, dim.d + dim.c / 2 - 0.7f * dim.r));
+
+            return result;
+        }
+
         float Length { get; set; }
         float Height { get; set; }
         float Offset { get; set; }
@@ -65,6 +86,7 @@ namespace Enveloper
             }
             return intPart + floatPart;
         }
+        
 
     }
 }
